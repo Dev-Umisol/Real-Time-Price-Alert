a2442e529cc8274f50bd2540e48e96825c2194c6
Dev-Umisol <destin.nguyen98@gmail.com>
2026-06-16 16:49:02 -0400

    feat: added classes UserRegistration, UserResponse, and UserLogin: each schemas has user_name and user_password, along with the response having the id

--- App/database/schemas.py
+++ App/database/schemas.py
@@ -1,3 +1,21 @@
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
+# Schemas for user registration and login
+class UserRegistration(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    user_name: str
+    user_password: str
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    user_name: str
+    
+class UserLogin(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    user_name: str
+    user_password: str
