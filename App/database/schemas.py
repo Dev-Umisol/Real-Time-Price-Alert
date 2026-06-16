2b748cc0e1d97c5a5834f70cdb88d306cad811d2
Dev-Umisol <destin.nguyen98@gmail.com>
2026-06-16 16:54:29 -0400

    feat: added UserToken, and UserAlertReponse: schemas for the users jwt token, and the useralert for coin

--- App/database/schemas.py
+++ App/database/schemas.py
@@ -8,14 +8,34 @@ class UserRegistration(BaseModel):
     user_name: str
     user_password: str
 
+# Schema for user response after registration or login
 class UserResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
     user_name: str
-    
+
+# Schema for user login
 class UserLogin(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     user_name: str
     user_password: str
+
+# Schema for token response after successful login
+class UserToken(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    access_token: str
+    token_type: str
+
+# Schema for creating a new alert
+class UserAlertResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    user_id: int
+    alert_id: int
+    coin_name: str
+    coin_price_threshold: float
+    alert_fired_at: None | datetime
+    is_active: bool
