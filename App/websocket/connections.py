5dd202034fa1c93ccf9d7321d4164e97173aa986
Dev-Umisol <destin.nguyen98@gmail.com>
2026-07-05 15:44:01 -0400

    feat: added function disconnect, will get called from main to disconnect the client_id from websocket

--- App/websocket/connections.py
+++ App/websocket/connections.py
@@ -1,5 +1,6 @@
 from fastapi import WebSocket
 
+# Allows new connections to be accepted and stored in a dictionary, where each connection is associated with a unique client ID.
 class ConnectionManager:
     # Manages active WebSocket connections
     def __init__(self):
@@ -9,4 +10,9 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket, client_id: str) -> None:
         await websocket.accept()
         self.active_connections[client_id] = websocket
+    
+    def disconnect(self, client_id: str) -> None:
+        # Removes a WebSocket connection from the active connections dictionary
+        if client_id in self.active_connections:
+            del self.active_connections[client_id]
     
